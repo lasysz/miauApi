5e980475a7ed48e61cd7ec7e6d1657d9c1510d5c
lasysz <laistenorio7@gmail.com>
2025-05-09 10:46:41 -0300

    Adicionar arquivos de projeto.

---
+++ MiauWebApiPiaget/Data/MiauWebApiPiagetContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Entities;
+
+namespace MiauWebApiPiaget.Data
+{
+    public class MiauWebApiPiagetContext : DbContext
+    {
+        public MiauWebApiPiagetContext (DbContextOptions<MiauWebApiPiagetContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Entities.Aluno> Aluno { get; set; } = default!;
+    }
+}
---
+++ MiauWebApiPiaget/Program.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Identity.Web;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using MiauWebApiPiaget.Data;
+
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContext<MiauWebApiPiagetContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("MiauWebApi") ?? throw new InvalidOperationException("Connection string 'MiauWebApiPiagetContext' not found.")));
+
+// Add services to the container.
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAd"));
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
